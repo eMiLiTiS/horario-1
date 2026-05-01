29bd8a2c971b33bbf76b001d323f43827f2c5c14
EmilioCocera <ecocera@campuscamarafp.com>
2026-05-01 16:46:34 +0200

    disable pwa ++ temporarily

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { VitePWA } from 'vite-plugin-pwa'
+//import { VitePWA } from 'vite-plugin-pwa'
 import path from 'path'
 
 export default defineConfig({
@@ -41,8 +41,8 @@ export default defineConfig({
         ],
       },
     }),
-  ],
-  */
+    */
+  ],  
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
